f1b38f0a4cb65998092ba5d4785c9be664376b26
andrey13 <andrey13@yandex.rum>
2023-10-14 13:26:43 +0300

    p 55

--- app/src/main/java/com/example/ordernow/main/OrderNowScreen.kt
+++ app/src/main/java/com/example/ordernow/main/OrderNowScreen.kt
@@ -1,30 +1,44 @@
 package com.example.ordernow.main
 
+import android.content.res.Resources
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import com.example.ordernow.ui.patterns.OrderNowBottomBar
 import com.example.ordernow.ui.patterns.OrderNowTopBar
 import com.example.ordernow.ui.theme.OrderNowTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
+
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun OrderNowScreen() {
     OrderNowTheme {
         Surface(
             modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
+            color = MaterialTheme.colors.background
         ) {
+            val appState = rememberAppState()
             Scaffold(
+                scaffoldState = appState.scaffoldState,
                 topBar = { OrderNowTopBar() },
                 bottomBar = { OrderNowBottomBar() }
-            ) { innerPaddingModifier ->
-                println(innerPaddingModifier)
+            ) { contentPadding ->
+                println(contentPadding)
             }
         }
     }
+}
+
+@Composable
+@ReadOnlyComposable
+fun resources(): Resources {
+    LocalConfiguration.current
+    return  LocalContext.current.resources
 }
